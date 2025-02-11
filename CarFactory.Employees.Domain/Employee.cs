7e2decb54163b6feb6ff46bb9c4999dcfde5e86d
Kamil Pankowski <pankowski.kamil1989@gmail.com>
2025-02-11 21:28:10 +0100

    WIP: First phase of Employee Model

---
+++ CarFactory.Employees.Domain/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CarFactory.Employees.Domain;
+
+public class Employee : BaseEntity
+{
+    public required string FirstName { get; set; }
+    public required string LastName { get; set; }
+    public DateTime StartEmploymentDate { get; set; }
+
+    [NotMapped]
+    public string FullName => $"{FirstName} {LastName}";
+    [NotMapped]
+    public string FullNameReverse => $"{LastName} {FirstName}";
+}
