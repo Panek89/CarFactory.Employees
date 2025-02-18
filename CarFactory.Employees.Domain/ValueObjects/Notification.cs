a64c7556d74a93b467d7e2cb9816d34f27279cda
Kamil Pankowski <pankowski.kamil1989@gmail.com>
2025-02-18 16:41:11 +0100

    WIP: Implement notification value object

---
+++ CarFactory.Employees.Domain/ValueObjects/Notification.cs
@@ -0,0 +1,8 @@
+﻿namespace CarFactory.Employees.Domain.ValueObjects;
+
+public class Notification
+{
+    public required string Title { get; set; }
+    public required string Desciption { get; set; }
+    public List<string> EMails { get; set; } = [];
+}
