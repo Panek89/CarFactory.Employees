d7a1f9afeb512a983b9d23a30e9484ec9487862d
Panek89 <pankowski.kamil1989@gmail.com>
2025-03-26 19:47:27 +0100

    WIP: Small changes in Base Entity

--- CarFactory.Employees.Domain/Models/BaseEntity.cs
+++ CarFactory.Employees.Domain/Models/BaseEntity.cs
@@ -4,7 +4,7 @@ public abstract class BaseEntity
 {
     public Guid Id { get; set; }
     public DateTime CreatedAt { get; set; }
-    public string CreatedBy { get; set; } = "SYSTEM";
+    public required string CreatedBy { get; set; }
     public DateTime? UpdatedAt { get; set; }
     public string? UpdatedBy { get; set; }
 }
