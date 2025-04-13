f2effc165f70e68c15b471780457285d962c3081
Kamil-T480 <pankowski.kamil1989@gmail.com>
2025-04-13 20:17:55 +0200

    WIP: Declare models for Employee Request and Employee Request Candidate

--- CarFactory.Employees.Domain/Models/EmployeeRequest.cs
+++ CarFactory.Employees.Domain/Models/EmployeeRequest.cs
@@ -1,6 +1,12 @@
-﻿namespace CarFactory.Employees.Domain.Models;
+﻿using CarFactory.Employees.SharedLibrary.Enums;
+
+namespace CarFactory.Employees.Domain.Models;
 
 public class EmployeeRequest : BaseEntity
 {
-
+    public int NoOfEmployeesNeeded { get; set; }
+    public required string Business { get; set; }
+    public DateTime StartDate { get; set; }
+    public EmployeeRequestStatus Status { get; set; }
+    public IEnumerable<EmployeeRequestCandidate> Candidates { get; set; } = [];
 }
---
+++ CarFactory.Employees.Domain/Models/EmployeeRequestCandidate.cs
@@ -0,0 +1,12 @@
+﻿using CarFactory.Employees.SharedLibrary.Enums;
+
+namespace CarFactory.Employees.Domain.Models;
+
+public class EmployeeRequestCandidate
+{
+    public required string FirstName { get; set; }
+    public required string LastName { get; set; }
+    public required string PersonalId { get; set; }
+    public required int Age { get; set; }
+    public EmployeeCandidateStatus Status { get; set; }
+}
