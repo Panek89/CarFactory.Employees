c9c3aef99ec1ac262edb15c2aaab533bc57aa90e
Kamil-T480 <pankowski.kamil1989@gmail.com>
2025-04-13 20:13:40 +0200

    WIP: Extend Base Entity model

--- CarFactory.Employees.Domain/Models/BaseEntity.cs
+++ CarFactory.Employees.Domain/Models/BaseEntity.cs
@@ -3,6 +3,7 @@
 public abstract class BaseEntity
 {
     public Guid Id { get; set; }
+    public bool IsDeleted { get; set; }
     public DateTime CreatedAt { get; set; }
     public required string CreatedBy { get; set; }
     public DateTime? UpdatedAt { get; set; }
