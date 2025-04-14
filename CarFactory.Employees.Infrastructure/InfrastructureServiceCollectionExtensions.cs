d8e7a28fbef0951e027ffa691dec3136fab2ca9c
Kamil-T480 <pankowski.kamil1989@gmail.com>
2025-04-14 20:56:24 +0200

    WIP: EfCore reference in infrastructure layer, register Infrastructure Extensions

---
+++ CarFactory.Employees.Infrastructure/InfrastructureServiceCollectionExtensions.cs
@@ -0,0 +1,22 @@
+﻿using CarFactory.Employees.Infrastructure.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CarFactory.Employees.Infrastructure;
+
+public static class InfrastructureServiceCollectionExtensions
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+    {
+        services.AddScoped<IEmployeeRequestRepository, EmployeeRequestRepository>();
+
+        return services;
+    }
+
+    public static IServiceCollection AddDatabase(this IServiceCollection services,
+       string connectionString)
+    {
+        // TODO
+
+        return services;
+    }
+}
