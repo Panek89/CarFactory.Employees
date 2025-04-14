18aa0d7f37027046db0fbc47720d1cca4f8e2dd2
Kamil-T480 <pankowski.kamil1989@gmail.com>
2025-04-14 20:58:49 +0200

    WIP: Employee Request DTO implementation and Contract Mapping

---
+++ CarFactory.Employees.Application/Features/EmployeeRequests/DTOs/EmployeeRequestContractMapping.cs
@@ -0,0 +1,34 @@
+﻿using CarFactory.Employees.Application.Features.EmployeeRequests.Commands;
+using CarFactory.Employees.Domain.Models;
+
+namespace CarFactory.Employees.Application.Features.EmployeeRequests.DTOs;
+
+public static class EmployeeRequestContractMapping
+{
+    public static EmployeeRequest MapToEmployeeRequest(this RegisterEmployeeRequestQuery query)
+    {
+        return new EmployeeRequest()
+        {
+            Id = Guid.NewGuid(),
+            NoOfEmployeesNeeded = query.NoOfEmployeesNeeded,
+            Business = query.Business,
+            StartDate = query.StartDate,
+            Status = query.Status,
+            IsDeleted = false,
+            CreatedAt = DateTime.UtcNow,
+            CreatedBy = "SYSTEM"
+        };
+    }
+
+    public static EmployeeRequestDto MapToDto(this EmployeeRequest employeeRequest)
+    {
+        return new EmployeeRequestDto()
+        {
+            Id = employeeRequest.Id,
+            NoOfEmployeesNeeded = employeeRequest.NoOfEmployeesNeeded,
+            Business = employeeRequest.Business,
+            StartDate = employeeRequest.StartDate,
+            Status = employeeRequest.Status
+        };
+    }
+}
---
+++ CarFactory.Employees.Application/Features/EmployeeRequests/DTOs/EmployeeRequestDto.cs
@@ -0,0 +1,12 @@
+﻿using CarFactory.Employees.SharedLibrary.Enums;
+
+namespace CarFactory.Employees.Application.Features.EmployeeRequests.DTOs;
+
+public class EmployeeRequestDto
+{
+    public Guid Id { get; init; }
+    public int NoOfEmployeesNeeded { get; init; }
+    public required string Business { get; init; }
+    public DateTime StartDate { get; init; }
+    public EmployeeRequestStatus Status { get; init; }
+}
