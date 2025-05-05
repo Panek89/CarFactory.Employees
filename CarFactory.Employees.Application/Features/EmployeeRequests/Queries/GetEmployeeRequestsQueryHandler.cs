dddf257e192c3032582f7b7be5cfe2a97805fefd
Kamil Pankowski <pankowski.kamil1989@gmail.com>
2025-05-05 19:17:44 +0200

    WIP: Move Repositories to Domain layer for avoid reference between Application and Domain layers

--- CarFactory.Employees.Application/Features/EmployeeRequests/Queries/GetEmployeeRequestsQueryHandler.cs
+++ CarFactory.Employees.Application/Features/EmployeeRequests/Queries/GetEmployeeRequestsQueryHandler.cs
@@ -1,5 +1,5 @@
 ﻿using CarFactory.Employees.Application.Features.EmployeeRequests.DTOs;
-using CarFactory.Employees.Infrastructure.Repositories;
+using CarFactory.Employees.Domain.Repositories;
 using MediatR;
 
 namespace CarFactory.Employees.Application.Features.EmployeeRequests.Queries;
--- CarFactory.Employees.Infrastructure/Repositories/BaseRepository.cs
+++ CarFactory.Employees.Infrastructure/Repositories/BaseRepository.cs
@@ -1,18 +1,8 @@
 ﻿using CarFactory.Employees.Domain.Models;
+using CarFactory.Employees.Domain.Repositories;
 using Microsoft.EntityFrameworkCore;
 
 namespace CarFactory.Employees.Infrastructure.Repositories;
-
-public interface IBaseRepository<T> where T : BaseEntity
-{
-    Task<IEnumerable<T>> GetAllAsync(CancellationToken token);
-    Task<T?> GetByIdAsync(Guid id, CancellationToken token);
-    Task AddAsync(T entity, CancellationToken token);
-    void Update(T entity);
-    void Delete(T entity);
-    Task SaveChangesAsync(CancellationToken token);
-    Task<bool> ExistsByIdAsync(Guid id, CancellationToken token);
-}
 
 public class BaseRepository<T> : IBaseRepository<T> where T : BaseEntity
 {
