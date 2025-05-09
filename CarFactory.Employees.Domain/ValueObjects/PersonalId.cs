88dd5a6a0fa8e95d8fc3640617579eeab5c88ce9
Kamil Pankowski <pankowski.kamil1989@gmail.com>
2025-05-09 22:35:35 +0200

    WIP: Green Tests for PersonalID value object

--- CarFactory.Employees.Domain/ValueObjects/PersonalId.cs
+++ CarFactory.Employees.Domain/ValueObjects/PersonalId.cs
@@ -1,4 +1,4 @@
-using System;
+using CarFactory.Employees.SharedLibrary.Extensions;
 
 namespace CarFactory.Employees.Domain.ValueObjects;
 
@@ -11,6 +11,16 @@ public class PersonalId
         if (value == default)
         {
             throw new ArgumentNullException(nameof(value), "Personal ID cannot be empty");
+        }
+
+        if (value.Length != 11)
+        {
+            throw new ArgumentException(nameof(value), "Personal ID length must be exact 11");
+        }
+
+        if (value.HasNonDigitChars())
+        {
+            throw new ArgumentException(nameof(value), "Personal ID can contains only digits");
         }
 
         Value = value;
