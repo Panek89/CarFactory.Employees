077f6d3c11f95b8f92153b3c91d67feed22e3033
Kamil-T480 <pankowski.kamil1989@gmail.com>
2025-05-11 20:14:43 +0200

    WIP: EmployeeRequest Extensions for Tests

---
+++ CarFactory.Employees.Domain.TESTS/Extensions/EmployeeRequestExtensions.cs
@@ -0,0 +1,15 @@
+﻿using CarFactory.Employees.Domain.Models;
+
+namespace CarFactory.Employees.Domain.TESTS.Extensions;
+
+public static class EmployeeRequestExtensions
+{
+    private static readonly int _correctNumberOfEmployeesNeeded = 5;
+    private static readonly string _correctBusiness = "Business";
+    private static readonly DateTime _correctStartDate = DateTime.UtcNow.AddMonths(2);
+
+    public static EmployeeRequest CorrectRegister()
+    {
+        return EmployeeRequest.Register(_correctNumberOfEmployeesNeeded, _correctBusiness, _correctStartDate);
+    }
+}
