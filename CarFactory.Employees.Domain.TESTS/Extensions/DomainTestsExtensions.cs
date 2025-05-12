5105fa3ca0e3ce5ccfa93e600af3e340514776a4
Kamil-T480 <pankowski.kamil1989@gmail.com>
2025-05-12 21:23:20 +0200

    WIP: Domain Tests Extension for correct register Employee Request Candidate

--- CarFactory.Employees.Domain.TESTS/Extensions/DomainTestsExtensions.cs
+++ CarFactory.Employees.Domain.TESTS/Extensions/DomainTestsExtensions.cs
@@ -1,4 +1,6 @@
 ﻿using CarFactory.Employees.Domain.Models;
+using CarFactory.Employees.Domain.ValueObjects;
+using CarFactory.Employees.SharedLibrary.Enums;
 
 namespace CarFactory.Employees.Domain.TESTS.Extensions;
 
@@ -8,8 +10,19 @@ public static class DomainTestsExtensions
     private static readonly string _correctBusiness = "Business";
     private static readonly DateTime _correctStartDate = DateTime.UtcNow.AddMonths(2);
 
+    private static readonly string _correctFirstName = "Imie";
+    private static readonly string _correctLastName = "LastName";
+    private static readonly PersonalId _correctPersonalId = new("12345678901");
+    private static readonly DateTime _correctDateOfBirth = DateTime.Today.AddYears(-20);
+    private static readonly Gender _maleGender = Gender.Male;
+
     public static EmployeeRequest EmployeeRequestRegisterCorrect()
     {
         return EmployeeRequest.Register(_correctNumberOfEmployeesNeeded, _correctBusiness, _correctStartDate);
+    }
+
+    public static EmployeeRequestCandidate EmployeeRequestCandidateRegisterCorrect()
+    {
+        return EmployeeRequestCandidate.RegisterCandidate(_correctFirstName, _correctLastName, _correctPersonalId, _correctDateOfBirth, _maleGender, EmployeeRequestRegisterCorrect());
     }
 }
