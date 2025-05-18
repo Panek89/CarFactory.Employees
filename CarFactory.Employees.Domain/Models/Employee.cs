2a8259468436a1e975dcd51fc138f63334f4da00
Kamil-T480 <pankowski.kamil1989@gmail.com>
2025-05-18 20:03:30 +0200

    WIP: Reimplement Employee model for start handling new DDD approach

--- CarFactory.Employees.Domain/Models/Employee.cs
+++ CarFactory.Employees.Domain/Models/Employee.cs
@@ -1,4 +1,5 @@
-﻿using CarFactory.Employees.Domain.ValueObjects;
+﻿using CarFactory.Employees.Domain.ExtensionMethods;
+using CarFactory.Employees.Domain.ValueObjects;
 using CarFactory.Employees.SharedLibrary.Enums;
 using System.ComponentModel.DataAnnotations.Schema;
 
@@ -6,62 +7,24 @@ namespace CarFactory.Employees.Domain.Models;
 
 public class Employee : BaseEntity
 {
-    private DateTime _employmentStartDate;
-    private DateTime? _employmentEndDate;
-    //private DateTime? _dateOfBirth;
-
-    public required string FirstName { get; set; }
-    public required string LastName { get; set; }
-    public required PersonalId PersonalId { get; set; }
-    public bool IsEmployed { get; set; }
-
-    public DateTime EmploymentStartDate 
-    { 
-        get { return _employmentStartDate; }  
-        set
-        {
-            if (_employmentStartDate == DateTime.MinValue)
-            {
-                throw new ArgumentException("Employment start date can't be Minimum Value", nameof(_employmentStartDate));
-            }
-
-            if (_employmentStartDate == DateTime.MaxValue)
-            {
-                throw new ArgumentException("Employment start date can't be Maximum Value", nameof(_employmentStartDate));
-            }
-
-            if (_employmentEndDate.HasValue && _employmentStartDate < _employmentEndDate)
-            {
-                throw new ArgumentException("Employment start date can't be earlier than _employmentEndData", nameof(_employmentStartDate));
-            }
-            _employmentStartDate = value;
-        }
-    }
-
-    public DateTime? EmploymentEndDate
-    {
-        get { return _employmentEndDate; }
-        set
-        {
-            if (_employmentStartDate == DateTime.MinValue)
-            {
-                throw new ArgumentException("Employment end date can't be Minimum Value", nameof(_employmentEndDate));
-            }
-
-            if (_employmentStartDate == DateTime.MaxValue)
-            {
-                throw new ArgumentException("Employment end date can't be Maximum Value", nameof(_employmentEndDate));
-            }
-
-            _employmentEndDate = value;
-        }
-    }
-
-    public DateTime DateOfBirth {get; set; }
-    public Gender Gender { get; set; }
+    public string FirstName { get; private set; } = null!;
+    public string LastName { get; private set; } = null!;
+    public PersonalId PersonalId { get; private set; } = null!;
+    public DateTime DateOfBirth { get; private set; }
+    public Gender Gender { get; private set; }
+    public bool IsEmployed { get; private set; }
+    public DateTime EmploymentStartDate { get; private set; }
+    public DateTime? EmploymentEndDate { get; private set; }
 
     [NotMapped]
     public string FullName => $"{FirstName} {LastName}";
     [NotMapped]
     public string FullNameReverse => $"{LastName} {FirstName}";
+
+    private Employee() {}
+
+    public static Employee Hire()
+    {
+        return new Employee().SetInitialMetaData();
+    }
 }
