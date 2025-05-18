962711eec753b4c0c4ac1fa27135077f5d7fa71d
Kamil-T480 <pankowski.kamil1989@gmail.com>
2025-05-18 20:22:09 +0200

    WIP: Implement Employee model extensions for tests

---
+++ CarFactory.Employees.Domain.TESTS/Extensions/EmployeeTestsExtensions.cs
@@ -0,0 +1,20 @@
+using CarFactory.Employees.Domain.Models;
+using CarFactory.Employees.Domain.ValueObjects;
+using CarFactory.Employees.SharedLibrary.Enums;
+
+namespace CarFactory.Employees.Domain.TESTS.Extensions;
+
+public static class EmployeeTestsExtensions
+{
+    private static readonly string _correctFirstName = "Imie";
+    private static readonly string _correctLastName = "Nazwisko";
+    private static readonly PersonalId _correctPersonalId = new("12345678901");
+    private static readonly DateTime _correctDateOfBirth = DateTime.Today.AddYears(-20);
+    private static readonly Gender _maleGender = Gender.Male;
+    private static readonly DateTime _correctEmploymentStartDate = DateTime.Today.AddDays(1);
+
+    public static Employee CorrectHire()
+    {
+        return Employee.Hire();
+    }
+}
