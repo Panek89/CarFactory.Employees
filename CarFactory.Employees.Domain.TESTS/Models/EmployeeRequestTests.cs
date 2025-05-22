f9794f9bc1209eb859c69c8b7cb4a09065a0091d
Panek89 <pankowski.kamil1989@gmail.com>
2025-05-22 15:12:53 +0200

    WIP: EmployeeRequest model implementation adjust to new approach with testas and factory

--- CarFactory.Employees.Domain.TESTS/Models/EmployeeRequestTests.cs
+++ CarFactory.Employees.Domain.TESTS/Models/EmployeeRequestTests.cs
@@ -1,5 +1,5 @@
-﻿using CarFactory.Employees.Domain.TESTS.Extensions;
-using CarFactory.Employees.SharedLibrary.Enums;
+﻿using CarFactory.Employees.Domain.Models;
+using CarFactory.Employees.Domain.TESTS.Factories;
 
 namespace CarFactory.Employees.Domain.TESTS.Models;
 
@@ -10,8 +10,8 @@ public class EmployeeRequestTests
     [TestCase(-1)]
     public void EmployeeRequest_ShouldThrowArgumentException_WhenNumberOfEmployeesNeeded_IsBelowOne(int numberOfEmployeesNeeded)
     {
-        Assert.That(() => DomainTestsExtensions.EmployeeRequestRegisterCorrect().SetNumberOfEmployeesNeeded(numberOfEmployeesNeeded),
-            Throws.ArgumentException.With.Property(nameof(ArgumentException.ParamName)).EqualTo("NumberOfEmployeesNeeded"));
+        Assert.That(() => EmployeeRequestFactory.Register(numberOfEmployeesNeeded),
+            Throws.ArgumentException.With.Property(nameof(ArgumentException.ParamName)).EqualTo(nameof(EmployeeRequest.NumberOfEmployeesNeeded)));
     }
 
     [TestCase(11)]
@@ -19,8 +19,8 @@ public class EmployeeRequestTests
     [TestCase(21)]
     public void EmployeeRequest_ShouldThrowArgumentException_WhenNumberOfEmployeesNeeded_IsAboveTen(int numberOfEmployeesNeeded)
     {
-        Assert.That(() => DomainTestsExtensions.EmployeeRequestRegisterCorrect().SetNumberOfEmployeesNeeded(numberOfEmployeesNeeded),
-            Throws.ArgumentException.With.Property(nameof(ArgumentException.ParamName)).EqualTo("NumberOfEmployeesNeeded"));
+        Assert.That(() => EmployeeRequestFactory.Register(numberOfEmployeesNeeded),
+            Throws.ArgumentException.With.Property(nameof(ArgumentException.ParamName)).EqualTo(nameof(EmployeeRequest.NumberOfEmployeesNeeded)));
     }
 
     [TestCase(null)]
@@ -28,45 +28,21 @@ public class EmployeeRequestTests
     [TestCase("")]
     public void EmployeeRequest_ShouldThrowArgumentNullException_WhenBusiness_IsNull(string? business)
     {
-        Assert.That(() => DomainTestsExtensions.EmployeeRequestRegisterCorrect().SetBusiness(business!),
-            Throws.ArgumentNullException.With.Property(nameof(ArgumentNullException.ParamName)).EqualTo("Business"));
+        Assert.That(() => EmployeeRequestFactory.RegisterNullBusiness(business: business),
+            Throws.ArgumentNullException.With.Property(nameof(ArgumentNullException.ParamName)).EqualTo(nameof(EmployeeRequest.Business)));
     }
 
     [Test]
     public void EmployeeRequest_ShouldThrowArgumentException_WhenStartDate_IsInThePast()
     {
-        Assert.That(() => DomainTestsExtensions.EmployeeRequestRegisterCorrect().SetStartDate(DateTime.Today.AddDays(-1)),
-            Throws.ArgumentException.With.Property(nameof(ArgumentException.ParamName)).EqualTo("StartDate"));
+        Assert.That(() => EmployeeRequestFactory.Register(startDate: DateTime.Today.AddDays(-1)),
+            Throws.ArgumentException.With.Property(nameof(ArgumentException.ParamName)).EqualTo(nameof(EmployeeRequest.StartDate)));
     }
 
     [Test]
     public void EmployeeRequest_ShouldThrowArgumentException_WhenStartDate_IsLessThanOneMonthInFuture()
     {
-        Assert.That(() => DomainTestsExtensions.EmployeeRequestRegisterCorrect().SetStartDate(DateTime.Today.AddMonths(1).AddDays(-1)),
-               Throws.ArgumentException.With.Property(nameof(ArgumentException.ParamName)).EqualTo("StartDate"));
+        Assert.That(() => EmployeeRequestFactory.Register(startDate:DateTime.Today.AddMonths(1).AddDays(-1)),
+               Throws.ArgumentException.With.Property(nameof(ArgumentException.ParamName)).EqualTo(nameof(EmployeeRequest.StartDate)));
     }
-
-    [TestCase(EmployeeRequestStatus.Approved)]
-    [TestCase(EmployeeRequestStatus.RejectedByHr)]
-    [TestCase(EmployeeRequestStatus.NoCandidates)]
-    [TestCase(EmployeeRequestStatus.CandidatesInInterview)]
-    [TestCase(EmployeeRequestStatus.CandidatesInExams)]
-    [TestCase(EmployeeRequestStatus.Completed)]
-    public void EmployeeRequest_ShouldThrowArgumentException_WhenStatusOnRegister_IsIncorrect(EmployeeRequestStatus status)
-    {
-        Assert.That(() => DomainTestsExtensions.EmployeeRequestRegisterCorrect().SetStatus(status),
-                Throws.ArgumentException.With.Property(nameof(ArgumentException.ParamName)).EqualTo("Status"));
-    }
-
-    // TODO
-    //[TestCase(1, 2)]
-    //[TestCase(5, 6)]
-    //[TestCase(7, 10)]
-    //public void EmployeeRequest_ShouldThrowArgumentException_WhenNumberOfCandidates_ExceedNumberOfEmployees(int numberOfEmployees, int numberOfCandidates)
-    //{
-    //    var fixture = new Fixture();
-    //    var candidates = fixture.CreateMany<EmployeeRequestCandidate>(numberOfCandidates);
-
-    //    Assert.Throws<ArgumentException>(() => CreateEmployeeRequest(numberOfEmployees, _correctBusiness, _correctStartDate, candidates));
-    //}
 }
--- CarFactory.Employees.Domain/Models/EmployeeRequest.cs
+++ CarFactory.Employees.Domain/Models/EmployeeRequest.cs
@@ -5,70 +5,94 @@ namespace CarFactory.Employees.Domain.Models;
 
 public class EmployeeRequest : BaseEntity
 {
-    public int NumberOfEmployeesNeeded { get; private set; }
-    public string Business { get; private set; } = null!;
-    public DateTime StartDate { get; private set; }
-    public EmployeeRequestStatus Status { get; private set; }
+    private int _numberOfEmployeesNeeded;
+    private string _business = null!;
+    private DateTime _startDate;
+    private EmployeeRequestStatus _status;
+
+    public int NumberOfEmployeesNeeded
+    {
+        get => _numberOfEmployeesNeeded;
+        private set
+        {
+            if (value < 1 || value > 10)
+            {
+                throw new ArgumentException("Number of employees needed in Request must be between 1 and 10", nameof(NumberOfEmployeesNeeded));
+            }
+
+            _numberOfEmployeesNeeded = value;
+        }
+    }
+    public string Business
+    {
+        get => _business;
+        private set
+        {
+            if (string.IsNullOrEmpty(value))
+            {
+                throw new ArgumentNullException(nameof(Business), "Business must be specified");
+            }
+
+            _business = value;
+        }
+    }
+    public DateTime StartDate
+    {
+        get => _startDate;
+        private set
+        {
+            if (value < DateTime.Today)
+            {
+                throw new ArgumentException("Request Start Date cannot be in the past", nameof(StartDate));
+            }
+
+            if (value < DateTime.Today.AddMonths(1))
+            {
+                throw new ArgumentException("Request Start Date must be at least one month in advance", nameof(StartDate));
+            }
+
+            _startDate = value;
+        }
+    }
+    public EmployeeRequestStatus Status
+    {
+        get => _status;
+        private set
+        {
+            if (value != EmployeeRequestStatus.Registered)
+            {
+                throw new ArgumentException($"On request registration, status can be only set to {EmployeeRequestStatus.Registered.ToString()}", nameof(Status));
+            }
+
+            _status = value;
+        }
+    }
     public ICollection<EmployeeRequestCandidate> Candidates { get; set; } = [];
 
     private EmployeeRequest() { }
 
-    public EmployeeRequest SetNumberOfEmployeesNeeded(int numberOfEmployeesNeeded)
+    private EmployeeRequest
+    (
+        int numberOfEmployeesNeeded,
+        string business,
+        DateTime startDate,
+        EmployeeRequestStatus status
+    )
     {
-        if (numberOfEmployeesNeeded < 1|| numberOfEmployeesNeeded > 10)
-        {
-            throw new ArgumentException("Number of employees needed in Request must be between 1 and 10", nameof(NumberOfEmployeesNeeded));
-        }
-
         NumberOfEmployeesNeeded = numberOfEmployeesNeeded;
-        return this;
-    }
-
-    public EmployeeRequest SetBusiness(string business)
-    {
-        if (string.IsNullOrEmpty(business))
-        {
-            throw new ArgumentNullException(nameof(Business), "Business must be specified");
-        }
-
         Business = business;
-        return this;
-    }
-
-    public EmployeeRequest SetStartDate(DateTime startDate)
-    {
-        if (startDate < DateTime.Today)
-        {
-            throw new ArgumentException("Request Start Date cannot be in the past", nameof(StartDate));
-        }
-
-        if (startDate < DateTime.Today.AddMonths(1))
-        {
-            throw new ArgumentException("Request Start Date must be at least one month in advance", nameof(StartDate));
-        }
-
         StartDate = startDate;
-        return this;
-    }
-
-    public EmployeeRequest SetStatus(EmployeeRequestStatus status)
-    {
-        if (status != EmployeeRequestStatus.Registered)
-        {
-            throw new ArgumentException($"On request registration, status can be only set to {EmployeeRequestStatus.Registered.ToString()}", nameof(Status));
-        }
-
         Status = status;
-        return this;
     }
 
     public static EmployeeRequest Register(int numberOfEmployeesNeeded, string business, DateTime startDate)
     {
-        return new EmployeeRequest()
-            .SetInitialMetaData()
-            .SetNumberOfEmployeesNeeded(numberOfEmployeesNeeded)
-            .SetBusiness(business)
-            .SetStartDate(startDate)
-            .SetStatus(EmployeeRequestStatus.Registered);
+        return new EmployeeRequest
+        (
+            numberOfEmployeesNeeded,
+            business,
+            startDate,
+            EmployeeRequestStatus.Registered
+        ).SetInitialMetaData();
     }
 }
