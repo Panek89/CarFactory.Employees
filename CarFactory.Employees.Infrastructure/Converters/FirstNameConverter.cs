e5ab7d17839dcbcd397dc4ee2e680c77485ae8a8
Kamil-T480 <pankowski.kamil1989@gmail.com>
2025-05-24 21:06:25 +0200

    WIP: Implement Value Converters for Value Objects in Models for EF Core

---
+++ CarFactory.Employees.Infrastructure/Converters/FirstNameConverter.cs
@@ -0,0 +1,12 @@
+﻿using CarFactory.Employees.Domain.ValueObjects;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+
+namespace CarFactory.Employees.Infrastructure.Converters;
+
+public class FirstNameConverter : ValueConverter<FirstName, string>
+{
+    public FirstNameConverter() 
+        : base(v => v.Value.ToString(), v => new FirstName(v))
+    {
+    }
+}
---
+++ CarFactory.Employees.Infrastructure/Converters/PersonalIdConverter.cs
@@ -0,0 +1,12 @@
+﻿using CarFactory.Employees.Domain.ValueObjects;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+
+namespace CarFactory.Employees.Infrastructure.Converters;
+
+public class PersonalIdConverter : ValueConverter<PersonalId, string>
+{
+    public PersonalIdConverter()
+        : base(v => v.Value.ToString(), v => new PersonalId(v)) 
+    { 
+    }
+}
