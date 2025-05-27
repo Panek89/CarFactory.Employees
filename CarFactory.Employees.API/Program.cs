66266ff0362522c29bcecfad349860041ecb5cfd
Kamil-T480 <pankowski.kamil1989@gmail.com>
2025-05-27 20:46:33 +0200

    WIP: Remove comments

--- CarFactory.Employees.API/Program.cs
+++ CarFactory.Employees.API/Program.cs
@@ -5,10 +5,7 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 builder.Services.AddSwaggerGen();
 builder.Services.AddInfrastructure();
@@ -17,7 +14,6 @@ builder.Services.AddApplication();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.RegisterDevelopment();
