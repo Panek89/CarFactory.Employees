1c9a5b4e5f62fc5dbcee6ceb55c8271249ce8dc5
Panek89 <pankowski.kamil1989@gmail.com>
2025-06-06 23:11:25 +0200

    WIP: Prepare function for fetch Employee Details

--- CarFactory.Employees.Domain/Repositories/IEmployeeRepository.cs
+++ CarFactory.Employees.Domain/Repositories/IEmployeeRepository.cs
@@ -1,8 +1,9 @@
 using CarFactory.Employees.Domain.Models;
+using CarFactory.Employees.Domain.ValueObjects;
 
 namespace CarFactory.Employees.Domain.Repositories;
 
 public interface IEmployeeRepository : IBaseRepository<Employee>
 {
-
+    Task<EmployeeDetails> GetEmployeeDetailsAsync(Guid id, CancellationToken token);
 }
--- CarFactory.Employees.Infrastructure/Repositories/EmployeeRepository.cs
+++ CarFactory.Employees.Infrastructure/Repositories/EmployeeRepository.cs
@@ -1,5 +1,7 @@
 using CarFactory.Employees.Domain.Models;
 using CarFactory.Employees.Domain.Repositories;
+using CarFactory.Employees.Domain.ValueObjects;
+using Microsoft.EntityFrameworkCore;
 
 namespace CarFactory.Employees.Infrastructure.Repositories;
 
@@ -7,5 +9,20 @@ public class EmployeeRepository : BaseRepository<Employee>, IEmployeeRepository
 {
     public EmployeeRepository(AppDbContext context) : base(context)
     {
+    }
+
+    public async Task<EmployeeDetails> GetEmployeeDetailsAsync(Guid id, CancellationToken token)
+    {
+        return await _dbSet
+            .AsNoTracking()
+            .Where(x => x.Id == id)
+            .Select(y => new EmployeeDetails
+                {
+                    FirstName = y.FirstName,
+                    LastName = y.LastName,
+                    PersonalId = y.PersonalId,
+                    DateOfBirth = y.DateOfBirth
+                })
+            .SingleAsync(token);
     }
 }
