6b29e28d5b17b5a868b9d0827f5479db5db85b9c
Kamil Pankowski <pankowski.kamil1989@gmail.com>
2025-06-11 18:25:04 +0200

    WIP: Extension method for register domain events and implement it on API layer

--- CarFactory.Employees.Application/ApplicationServiceCollectionExtensions.cs
+++ CarFactory.Employees.Application/ApplicationServiceCollectionExtensions.cs
@@ -1,4 +1,7 @@
-﻿using MediatR;
+﻿using CarFactory.Employees.Application.Features.EmployeeRequests.Events;
+using CarFactory.Employees.Domain.Common;
+using CarFactory.Employees.Domain.Events.EmployeeRequest;
+using MediatR;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace CarFactory.Employees.Application;
@@ -8,6 +11,13 @@ public static class ApplicationServiceCollectionExtensions
     public static IServiceCollection AddApplication(this IServiceCollection services)
     {
         services.AddMediatR(typeof(IApplicationMarker));
+
+        return services;
+    }
+
+    public static IServiceCollection RegisterEvents(this IServiceCollection services)
+    {
+        services.AddScoped<IDomainEventHandler<AssignedCandidateEvent>, AssignedCandidateEventHandler>();
 
         return services;
     }
