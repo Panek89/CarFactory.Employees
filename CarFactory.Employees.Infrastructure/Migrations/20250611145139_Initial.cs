e4b9dfb15e7c4bf101c62e1bf76bb4ac84b7b8e3
Kamil Pankowski <pankowski.kamil1989@gmail.com>
2025-06-11 17:05:38 +0200

    WIP: Regenerate migrations after EF Core configuration

--- CarFactory.Employees.Infrastructure/Migrations/20250611145139_Initial.cs
+++ CarFactory.Employees.Infrastructure/Migrations/20250611145139_Initial.cs
@@ -66,7 +66,7 @@ namespace CarFactory.Employees.Infrastructure.Migrations
                     Gender = table.Column<int>(type: "int", nullable: false),
                     DateOfBirth = table.Column<DateTime>(type: "datetime2", nullable: false),
                     Status = table.Column<int>(type: "int", nullable: false),
-                    EmployeeRequestId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    EMPLOYEE_REQUEST_ID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
                     IsDeleted = table.Column<bool>(type: "bit", nullable: false),
                     CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
                     CreatedBy = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
@@ -77,17 +77,17 @@ namespace CarFactory.Employees.Infrastructure.Migrations
                 {
                     table.PrimaryKey("PK_EMPLOYEE_REQUEST_CANIDATES", x => x.Id);
                     table.ForeignKey(
-                        name: "FK_EMPLOYEE_REQUEST_CANIDATES_EMPLOYEE_REQUESTS_EmployeeRequestId",
-                        column: x => x.EmployeeRequestId,
+                        name: "FK_EMPLOYEE_REQUEST_CANIDATES_EMPLOYEE_REQUESTS_EMPLOYEE_REQUEST_ID",
+                        column: x => x.EMPLOYEE_REQUEST_ID,
                         principalTable: "EMPLOYEE_REQUESTS",
                         principalColumn: "Id",
                         onDelete: ReferentialAction.Cascade);
                 });
 
             migrationBuilder.CreateIndex(
-                name: "IX_EMPLOYEE_REQUEST_CANIDATES_EmployeeRequestId",
+                name: "IX_EMPLOYEE_REQUEST_CANIDATES_EMPLOYEE_REQUEST_ID",
                 table: "EMPLOYEE_REQUEST_CANIDATES",
-                column: "EmployeeRequestId");
+                column: "EMPLOYEE_REQUEST_ID");
         }
 
         /// <inheritdoc />
