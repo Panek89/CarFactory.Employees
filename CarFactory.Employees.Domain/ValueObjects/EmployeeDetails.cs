2621faf4f26feae670f113bd0bf237bd904e5fa1
Kamil-T480 <pankowski.kamil1989@gmail.com>
2025-06-16 20:02:01 +0200

    WIP: Remove Value Objects, which should be DTOs

--- CarFactory.Employees.Domain/ValueObjects/EmployeeDetails.cs
+++
@@ -1,9 +0,0 @@
-namespace CarFactory.Employees.Domain.ValueObjects;
-
-public class EmployeeDetails
-{
-    public required string FirstName { get; init; }
-    public required string LastName { get; init; }
-    public required PersonalId PersonalId { get; init; }
-    public required DateTime DateOfBirth { get; init; }
-}
