f2f0346c87cd02909429bd3e49565e36cf81f8f4
Kamil-T480 <pankowski.kamil1989@gmail.com>
2025-06-16 20:08:38 +0200

    WIP: Remove Value Objects from Repositories

--- CarFactory.Employees.Domain/Repositories/IEmployeeRequestRepository.cs
+++ CarFactory.Employees.Domain/Repositories/IEmployeeRequestRepository.cs
@@ -1,11 +1,8 @@
 ﻿using CarFactory.Employees.Domain.Models;
-using CarFactory.Employees.Domain.ValueObjects;
 
 namespace CarFactory.Employees.Domain.Repositories;
 
 public interface IEmployeeRequestRepository : IBaseRepository<EmployeeRequest>
 {
-    Task<EmployeeRequest?> GetRequestWithCandidatesAsync(Guid id, CancellationToken cancellationToken);
-    Task<IEnumerable<EmployeeRequestDetails>> ActualRequestsDetailsAsync(CancellationToken token);
     Task<IEnumerable<EmployeeRequest>> GetAllWithCandidatesAsync(CancellationToken token);
 }
--- CarFactory.Employees.Infrastructure/Repositories/EmployeeRepository.cs
+++ CarFactory.Employees.Infrastructure/Repositories/EmployeeRepository.cs
@@ -1,8 +1,5 @@
-using CarFactory.Employees.Domain.Contracts;
 using CarFactory.Employees.Domain.Models;
 using CarFactory.Employees.Domain.Repositories;
-using CarFactory.Employees.Domain.ValueObjects;
-using Microsoft.EntityFrameworkCore;
 
 namespace CarFactory.Employees.Infrastructure.Repositories;
 
@@ -10,23 +7,5 @@ public class EmployeeRepository : BaseRepository<Employee>, IEmployeeRepository
 {
     public EmployeeRepository(AppDbContext context) : base(context)
     {
-    }
-
-    public async Task<EmployeeDetails?> GetEmployeeDetailsAsync(Guid id, CancellationToken token)
-    {
-        return await _dbSet
-            .AsNoTracking()
-            .Where(x => x.Id == id)
-            .Select(y => y.MapToEmployeeDetails())
-            .SingleOrDefaultAsync(token);
-    }
-
-    public async Task<EmployeeDetails?> GetEmployeeDetailsAsync(PersonalId personalId, CancellationToken token)
-    {
-        return await _dbSet
-            .AsNoTracking()
-            .Where(x => x.PersonalId == personalId)
-            .Select(y => y.MapToEmployeeDetails())
-            .SingleOrDefaultAsync(token);
     }
 }
