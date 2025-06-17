ba73f15daabeadabde40110f26aadd9207e32d03
Kamil Pankowski <pankowski.kamil1989@gmail.com>
2025-06-17 19:39:49 +0200

    WIP: Replace repository with service in get actual request query handler

--- CarFactory.Employees.Application/Features/EmployeeRequests/Queries/GetActualRequestesDetailsQueryHandler.cs
+++ CarFactory.Employees.Application/Features/EmployeeRequests/Queries/GetActualRequestesDetailsQueryHandler.cs
@@ -1,21 +1,20 @@
 using CarFactory.Employees.Application.Features.EmployeeRequests.DTOs;
-using CarFactory.Employees.Domain.Repositories;
 using MediatR;
 
 namespace CarFactory.Employees.Application.Features.EmployeeRequests.Queries;
 
 public class GetActualRequestesDetailsQueryHandler : IRequestHandler<GetActualRequestesDetailsQuery, IEnumerable<EmployeeRequestDetailsDto>>
 {
-    private readonly IEmployeeRequestRepository _employeeRequestRepository;
+    private readonly IEmployeeRequestService _employeeRequestService;
 
-    public GetActualRequestesDetailsQueryHandler(IEmployeeRequestRepository employeeRequestRepository)
+    public GetActualRequestesDetailsQueryHandler(IEmployeeRequestService employeeRequestService)
     {
-        _employeeRequestRepository = employeeRequestRepository ?? throw new ArgumentNullException(nameof(employeeRequestRepository));
+        _employeeRequestService = employeeRequestService ?? throw new ArgumentNullException(nameof(employeeRequestService));
     }
 
     public async Task<IEnumerable<EmployeeRequestDetailsDto>> Handle(GetActualRequestesDetailsQuery query, CancellationToken cancellationToken)
     {
-        return await _employeeRequestRepository.ActualRequestsDetailsAsync(cancellationToken);
+        return await _employeeRequestService.ActualRequestsDetailsAsync(cancellationToken);
     }
 }
 
