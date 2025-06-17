34bec28a9b0b63b1386134cd82da79b7f96b7912
Kamil Pankowski <pankowski.kamil1989@gmail.com>
2025-06-17 20:24:32 +0200

    WIP: Employee Service methods for fetch Employee Details DTO

--- CarFactory.Employees.Application/Features/Employees/IEmployeeService.cs
+++ CarFactory.Employees.Application/Features/Employees/IEmployeeService.cs
@@ -1,5 +1,10 @@
-﻿namespace CarFactory.Employees.Application.Features.Employees;
+﻿using CarFactory.Employees.Application.Features.Employees.DTOs;
+using CarFactory.Employees.Domain.ValueObjects;
+
+namespace CarFactory.Employees.Application.Features.Employees;
 
 public interface IEmployeeService
 {
+    Task<EmployeeDetailsDto?> GetEmployeeDetailsAsync(Guid id, CancellationToken token);
+    Task<EmployeeDetailsDto?> GetEmployeeDetailsAsync(PersonalId personalId, CancellationToken token);
 }
--- CarFactory.Employees.Infrastructure/Services/EmployeeService.cs
+++ CarFactory.Employees.Infrastructure/Services/EmployeeService.cs
@@ -1,5 +1,7 @@
 ﻿using CarFactory.Employees.Application.Features.Employees;
+using CarFactory.Employees.Application.Features.Employees.DTOs;
 using CarFactory.Employees.Domain.Models;
+using CarFactory.Employees.Domain.ValueObjects;
 using Microsoft.EntityFrameworkCore;
 
 namespace CarFactory.Employees.Infrastructure.Services;
@@ -11,5 +13,15 @@ public class EmployeeService : IEmployeeService
     public EmployeeService(AppDbContext context)
     {
         _employees = context.Employees;
+    }
+
+    public async Task<EmployeeDetailsDto?> GetEmployeeDetailsAsync(Guid id, CancellationToken token)
+    {
+        return await _employees.Where(x => x.Id == id).Select(s => s.MapToDto()).SingleOrDefaultAsync();
+    }
+
+    public async Task<EmployeeDetailsDto?> GetEmployeeDetailsAsync(PersonalId personalId, CancellationToken token)
+    {
+        return await _employees.Where(x => x.PersonalId == personalId).Select(s => s.MapToDto()).SingleOrDefaultAsync();
     }
 }
