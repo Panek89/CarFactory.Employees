0cccd64f1586686f155c6db5951a7b4d83d3aae5
Kamil Pankowski <pankowski.kamil1989@gmail.com>
2025-06-17 19:33:31 +0200

    WIP: Implement Employee Request service methods for fetch Employee Request details DTOs

--- CarFactory.Employees.Application/Features/EmployeeRequests/IEmployeeRequestService.cs
+++ CarFactory.Employees.Application/Features/EmployeeRequests/IEmployeeRequestService.cs
@@ -1,5 +1,9 @@
-﻿namespace CarFactory.Employees.Application.Features.EmployeeRequests;
+﻿using CarFactory.Employees.Application.Features.EmployeeRequests.DTOs;
+
+namespace CarFactory.Employees.Application.Features.EmployeeRequests;
 
 public interface IEmployeeRequestService
 {
+    Task<IEnumerable<EmployeeRequestDetailsDto>> ActualRequestsDetailsAsync(CancellationToken cancellationToken);
+    Task<EmployeeRequestDetailsDto?> GetRequestDetailsWithCandidatesAsync(Guid employeeRequestId, CancellationToken token);
 }
--- CarFactory.Employees.Infrastructure/Services/EmployeeRequestService.cs
+++ CarFactory.Employees.Infrastructure/Services/EmployeeRequestService.cs
@@ -1,15 +1,33 @@
 ﻿using CarFactory.Employees.Application.Features.EmployeeRequests;
+using CarFactory.Employees.Application.Features.EmployeeRequests.DTOs;
 using CarFactory.Employees.Domain.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace CarFactory.Employees.Infrastructure.Services;
 
-public class EmployeeRequestService: IEmployeeRequestService
+public class EmployeeRequestService : IEmployeeRequestService
 {
     private readonly DbSet<EmployeeRequest> _employeeRequests;
 
     public EmployeeRequestService(AppDbContext context)
     {
         _employeeRequests = context.EmployeeRequests;
+    }
+
+    public async Task<IEnumerable<EmployeeRequestDetailsDto>> ActualRequestsDetailsAsync(CancellationToken cancellationToken)
+    {
+        return await _employeeRequests.Where(x => x.StartDate > DateTime.Today)
+            .Select(s => s.MapToDto())
+            .AsNoTracking()
+            .ToListAsync();
+    }
+
+    public async Task<EmployeeRequestDetailsDto?> GetRequestDetailsWithCandidatesAsync(Guid employeeRequestId, CancellationToken token)
+    {
+        return await _employeeRequests.Where(x => x.Id == employeeRequestId)
+            .Include(i => i.Candidates)
+            .Select(s => s.MapToDto())
+            .AsNoTracking()
+            .SingleOrDefaultAsync(token);
     }
 }
