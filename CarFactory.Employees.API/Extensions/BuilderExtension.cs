64b6a214d21b30b0c6cc44883ebe2e58862a3a4f
Kamil-T480 <pankowski.kamil1989@gmail.com>
2025-07-23 21:13:32 +0200

    WIP: Declare Keycloak configuration for options pattern

--- CarFactory.Employees.API/Extensions/BuilderExtension.cs
+++ CarFactory.Employees.API/Extensions/BuilderExtension.cs
@@ -7,6 +7,7 @@ public static class BuilderExtensions
     public static WebApplicationBuilder ConfigureOptions(this WebApplicationBuilder builder)
     {
         builder.Services.Configure<AppSettingsConfiguration>(builder.Configuration.GetSection(AppSettingsConfiguration.AppSettings));
+        builder.Services.Configure<KeycloakConfiguration>(builder.Configuration.GetSection(KeycloakConfiguration.Keycloak));
 
         return builder;
     }
