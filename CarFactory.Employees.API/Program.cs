7111f25c0c0c537a9b1cef023138bab3dcd48107
Kamil-T480 <pankowski.kamil1989@gmail.com>
2025-07-25 21:55:20 +0200

    WIP: Configure  swagger for handling JWT Bearer tokens

--- CarFactory.Employees.API/Program.cs
+++ CarFactory.Employees.API/Program.cs
@@ -3,6 +3,7 @@ using CarFactory.Employees.Application;
 using CarFactory.Employees.Infrastructure;
 using CarFactory.Employees.SharedLibrary.Options;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,8 +11,6 @@ builder.ConfigureOptions();
 
 builder.Services.AddControllers();
 builder.Services.AddHttpClient();
-builder.Services.AddOpenApi();
-builder.Services.AddSwaggerGen();
 builder.Services.AddInfrastructure();
 builder.Services.AddDatabase(builder.Configuration.GetConnectionString("SQLSERVER")!);
 builder.Services.AddApplication();
@@ -19,6 +18,36 @@ builder.Services.RegisterEvents();
 builder.Services.RegisterMassTransit(
     builder.Configuration.GetSection(AppSettingsConfiguration.AppSettings).Get<AppSettingsConfiguration>()!);
 builder.Services.AddHealthChecks();
+builder.Services.AddOpenApi();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "CarFactory_Employees_API", Version = "v1" });
+
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Description = "Insert JWT access token",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.Http,
+        Scheme = "bearer",
+        BearerFormat = "JWT"
+    });
+
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            new string[] {}
+        }
+    });
+});
 
 builder.ConfigureJwt(
     builder.Configuration.GetSection(KeycloakConfiguration.Keycloak).Get<KeycloakConfiguration>()!
