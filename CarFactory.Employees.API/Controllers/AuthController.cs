5b588e617065d67e99dd8677f8a8ed34281227d7
Kamil-T480 <pankowski.kamil1989@gmail.com>
2025-07-25 21:54:50 +0200

    WIP: Add check access token get method

--- CarFactory.Employees.API/Controllers/AuthController.cs
+++ CarFactory.Employees.API/Controllers/AuthController.cs
@@ -1,42 +1,57 @@
+using System.IdentityModel.Tokens.Jwt;
 using CarFactory.Employees.SharedLibrary.Options;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
 
-namespace CarFactory.Employees.API.Controllers
+namespace CarFactory.Employees.API.Controllers;
+
+[Route("api/auth")]
+[ApiController]
+public class AuthController : ControllerBase
 {
-    [Route("api/auth")]
-    [ApiController]
-    public class AuthController : ControllerBase
+    private readonly HttpClient _httpClient;
+    private readonly KeycloakConfiguration _keycloakConfiguration;
+
+    public AuthController(IHttpClientFactory httpClientFactory, IOptions<KeycloakConfiguration> options)
     {
-        private readonly HttpClient _httpClient;
-        private readonly KeycloakConfiguration _keycloakConfiguration;
+        _httpClient = httpClientFactory.CreateClient() ?? throw new ArgumentNullException(nameof(_httpClient));
+        _keycloakConfiguration = options.Value;
+    }
 
-        public AuthController(IHttpClientFactory httpClientFactory, IOptions<KeycloakConfiguration> options)
+    [HttpPost("token")]
+    public async Task<IActionResult> GetToken([FromBody] LoginRequest request)
+    {
+        var parameters = new Dictionary<string, string>
         {
-            _httpClient = httpClientFactory.CreateClient() ?? throw new ArgumentNullException(nameof(_httpClient));
-            _keycloakConfiguration = options.Value;
-        }
+            ["client_id"] = _keycloakConfiguration.ClientId,
+            ["client_secret"] = _keycloakConfiguration.ClientSecret,
+            ["grant_type"] = _keycloakConfiguration.GrantType,
+            ["username"] = request.Username,
+            ["password"] = request.Password
+        };
 
-        [HttpPost("token")]
-        public async Task<IActionResult> GetToken([FromBody] LoginRequest request)
+        var url = $"http://{_keycloakConfiguration.Host}:{_keycloakConfiguration.Port}/realms/{_keycloakConfiguration.Realm}/protocol/openid-connect/token";
+        var content = new FormUrlEncodedContent(parameters);
+
+        var response = await _httpClient.PostAsync(url, content);
+        var responseBody = await response.Content.ReadAsStringAsync();
+
+        return Content(responseBody, "application/json");
+    }
+
+    [HttpGet("debug-token/{accessToken}")]
+    public IActionResult DebugToken(string accessToken)
+    {
+        var handler = new JwtSecurityTokenHandler();
+        var jsonToken = handler.ReadJwtToken(accessToken);
+
+        return Ok(new
         {
-            var parameters = new Dictionary<string, string>
-            {
-                ["client_id"] = _keycloakConfiguration.ClientId,
-                ["client_secret"] = _keycloakConfiguration.ClientSecret,
-                ["grant_type"] = _keycloakConfiguration.GrantType,
-                ["username"] = request.Username,
-                ["password"] = request.Password
-            };
-
-            var url = $"http://{_keycloakConfiguration.Host}:{_keycloakConfiguration.Port}/realms/{_keycloakConfiguration.Realm}/protocol/openid-connect/token";
-            var content = new FormUrlEncodedContent(parameters);
-            
-            var response = await _httpClient.PostAsync(url, content);
-            var responseBody = await response.Content.ReadAsStringAsync();
-
-            return Content(responseBody, "application/json");
-        }
+            Issuer = jsonToken.Issuer,
+            Audience = jsonToken.Audiences,
+            ValidTo = jsonToken.ValidTo,
+            Claims = jsonToken.Claims.Select(c => new { c.Type, c.Value })
+        });
     }
 }
 
