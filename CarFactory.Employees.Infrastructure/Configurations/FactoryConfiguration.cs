d41b03e7d74b23e45c626d4542e14ab330eac1f8
Panek89 <pankowski.kamil1989@gmail.com>
2025-07-31 22:04:50 +0200

    WIP: Factory entity configuration

---
+++ CarFactory.Employees.Infrastructure/Configurations/FactoryConfiguration.cs
@@ -0,0 +1,20 @@
+using CarFactory.Employees.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CarFactory.Employees.Infrastructure.Configurations;
+
+public class FactoryConfiguration : IEntityTypeConfiguration<Factory>
+{
+    public void Configure(EntityTypeBuilder<Factory> builder)
+    {
+        builder.ToTable("FACTORIES");
+
+        builder.HasKey(k => k.Id);
+
+        builder.Property(p => p.FactoryId).IsRequired();
+        builder.Property(p => p.Name).IsRequired().HasMaxLength(100);
+
+        builder.BaseEntityConfiguration();
+    }
+}
