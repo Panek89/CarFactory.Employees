f3d865f5166e9965666e1219435bc123b7143867
Panek89 <pankowski.kamil1989@gmail.com>
2025-08-04 20:38:38 +0200

    WIP: Adjust Created Fsctory event to persist Factory entity

--- CarFactory.Employees.Infrastructure/Consumers/FactoryCreatedConsumer.cs
+++ CarFactory.Employees.Infrastructure/Consumers/FactoryCreatedConsumer.cs
@@ -1,15 +1,32 @@
 using CarFactory.Employees.Contracts.Events;
+using CarFactory.Employees.Domain.Entities;
+using CarFactory.Employees.Domain.ExtensionMethods;
+using CarFactory.Employees.Domain.Repositories;
 using MassTransit;
 
 namespace CarFactory.Employees.Infrastructure.Consumers;
 
 public class FactoryCreatedConsumer : IConsumer<IFactoryCreated>
 {
-    public Task Consume(ConsumeContext<IFactoryCreated> context)
+    private readonly IFactoryRepository _factoryRepository;
+
+    public FactoryCreatedConsumer(IFactoryRepository factoryRepository)
+    {
+        _factoryRepository = factoryRepository ?? throw new ArgumentNullException(nameof(factoryRepository));
+    }
+
+    public async Task Consume(ConsumeContext<IFactoryCreated> context)
     {
         var message = context.Message;
-        Console.WriteLine($"New Fabric created: {message.Name} with number of employees: {message.NumberOfEmployees}");
+        var factory = new Factory()
+        {
+            FactoryId = message.Id,
+            Name = message.Name,
+        }.SetInitialMetaData();
 
-        return Task.CompletedTask;
+        await _factoryRepository.AddAsync(factory, context.CancellationToken);
+        await _factoryRepository.SaveChangesAsync(context.CancellationToken);
+
+        Console.WriteLine($"New Fabric created: {message.Name} with number of employees: {message.NumberOfEmployees}");
     }
 }
